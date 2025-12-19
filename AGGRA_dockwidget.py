2a1c6262533a57a1694537792a8de4bcf11a75cd
TEAkinboyewa <teakinboyewa@gmail.com>
2025-12-19 02:04:11 -0500

    Updated AGGRA_dockwidget.py

--- AGGRA_dockwidget.py
+++ AGGRA_dockwidget.py
@@ -817,7 +817,7 @@ class AGGRADockWidget(QtWidgets.QDockWidget, FORM_CLASS):
             elif model_name == 'gpt-5.2':
                 # GPT-5.2 supports: minimal, low, medium, high
                 effort_options = ['none', 'low','medium','high','xhigh']
-                default_effort = 'medium'
+                default_effort = 'low'
 
             # Update the combo box items
             # current_text = self.reasoningEffortComboBox.currentText()
